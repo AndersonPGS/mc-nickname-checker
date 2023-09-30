0a20a9ea55e7a18dfa89e132124ae4afbfaed987
AndersonPGS <andersonpgs2001@hotmail.com>
2023-09-30 17:42:13 -0300

    feat: Added a file to check if nick is tested

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const verifyUsername = require('./src/functions/verifyUsername');
 const nickGenerator = require('./src/functions/nickGenerator');
+const fs = require('fs');
 
 // Colors
 const RED = '\x1b[31m';
@@ -10,12 +11,33 @@ const RESET = '\x1b[0m';
 async function main() {
   console.log(GREEN + `Iniciando aplicação... | [AndersonPGS](https://github.com/AndersonPGS/mc-nickname-checker)` + RESET);
 
+  // Cria o arquivo checked.txt se não existir
+  if (!fs.existsSync('checked.txt')) {
+    fs.writeFileSync('checked.txt', '');
+  }
+
+  function loadCheckedNicks() {
+    try {
+      const data = fs.readFileSync('checked.txt', 'utf8');
+      const checkedNicks = data.split('\n').filter(Boolean);
+      return new Set(checkedNicks); // Usando um Set para evitar duplicatas
+    } catch (err) {
+      return new Set();
+    }
+  }
+  
+  const checkedNicksSet = loadCheckedNicks();
+
   const generator = nickGenerator();
   
   const generateAndVerify = async () => {
     const { value, done } = await generator.next();
     if (!done) {
-      await verifyUsername(value);
+      if (!checkedNicksSet.has(value)) {
+        await verifyUsername(value);
+        checkedNicksSet.add(value);
+        fs.appendFileSync('checked.txt', value + '\n');
+      }
       generateAndVerify();
     } else {
       console.log(GREEN + `Todos os nicks foram gerados e verificados.` + RESET);
